7d7eee2ede5bc413ca671949999cc2704264f127
tundenewcodes <fasasipelumi1652@gmail.com>
2026-04-26 21:20:47 +0100

    ticket module setup

--- apps/tickets/src/main.ts
+++ apps/tickets/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SERVICES_PORTS } from '@app/common';
 import { TicketsModule } from './tickets.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(TicketsModule);
-  await app.listen(process.env.port ?? 3000);
+
+  //Enable validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(SERVICES_PORTS.TICKETS_SERVICE);
+  console.log(
+    `Tickets Service is running on port ${SERVICES_PORTS.TICKETS_SERVICE}`,
+  );
 }
 bootstrap();
--- libs/common/src/dto/index.ts
+++ libs/common/src/dto/index.ts
@@ -1,3 +1,6 @@
 export * from './register.dto';
 export * from './login.dto';
 export * from './create-event-dto';
+export * from './update-event.dto';
+export * from './purchase-ticket.dto';
+export * from './check-in-dto';
